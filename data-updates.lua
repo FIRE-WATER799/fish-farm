c13f62dd4eb68024f2a8989333bc1ccc18ee14b9
FIRE-WATER799 <57648521+FIRE-WATER799@users.noreply.github.com>
2022-10-07 14:44:25 -0500

    Update data-updates.lua

--- data-updates.lua
+++ data-updates.lua
@@ -1,6 +1,6 @@
 if mods["Krastorio2"] then
     data.raw.recipe["fish-farm"].ingredients={{"raw-fish", 20}, {"glass", 25}, {"iron-plate", 10}}
 end
-if mods["aai-industry"] then
+if mods["aai-industry"] and not mods["Krastorio2"] then
     data.raw.recipe["fish-farm"].ingredients={{"raw-fish", 20}, {"glass", 25}, {"iron-plate", 10}}
 end
