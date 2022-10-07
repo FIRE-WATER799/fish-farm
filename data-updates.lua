da79a82d3fe96a24abc2f92000b97241c5fed45c
FIRE-WATER799 <57648521+FIRE-WATER799@users.noreply.github.com>
2022-10-07 14:43:07 -0500

    Update data-updates.lua

--- data-updates.lua
+++ data-updates.lua
@@ -1,7 +1,6 @@
-local ingredients = data.raw.recipe["fish-farm"].ingredients
 if mods["Krastorio2"] then
-    data.raw.recipe["fish-farm"].ingredients.insert(ingredients, {"glass", 25})
+    data.raw.recipe["fish-farm"].ingredients={{"raw-fish", 20}, {"glass", 25}, {"iron-plate", 10}}
 end
 if mods["aai-industry"] then
-    data.raw.recipe["fish-farm"].ingredients.insert(ingredients, {"glass", 25})
+    data.raw.recipe["fish-farm"].ingredients={{"raw-fish", 20}, {"glass", 25}, {"iron-plate", 10}}
 end
